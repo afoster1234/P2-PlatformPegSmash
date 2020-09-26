60869eebc43e12203ce2411976f8c142e773af99
Austin Foster <afoster@bsu.edu>
2020-09-26 00:11:14 -0400

    Player content added

--- project/src/Player.gd
+++ project/src/Player.gd
@@ -8,19 +8,15 @@ var velocity = Vector2.ZERO
 
 func _process(delta):
 	velocity.x = 0
-	#Movement X
 	if Input.is_action_pressed("move_right"):
 		velocity.x += SPEED
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= SPEED
 
-	#Movement Y
 	velocity.y += GRAVITY
 	if Input.is_action_just_pressed("jump"):
 		velocity.y -= JUMP
-
 	velocity = move_and_slide(velocity)
-
 	#Animations
 	if velocity.x != 0:
 		$AnimatedSprite.animation = "Walk"
